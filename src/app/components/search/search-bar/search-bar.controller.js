c50f4de91a4389908809277872241043939efdbc
mshan10 <mshan@nd.edu>
2018-06-08 22:12:23 -0400

    trying to use promises

--- src/app/components/search/search-bar/search-bar.controller.js
+++ src/app/components/search/search-bar/search-bar.controller.js
@@ -2,10 +2,13 @@ function SearchBarController(SearchService, $state) {
   var ctrl = this;
 
   ctrl.submitKeyword = function () {
-    // console.log(ctrl.keyword);
-    // return SearchService
-    //   .navParse(ctrl.searchInput)
-      console.log(SearchService.navParse(ctrl.searchInput));
+    console.log(ctrl.keyword);
+    return SearchService
+      .navParse(ctrl.searchInput)
+      .then(function() {
+        $state.go('searchResults')
+      })
+      // console.log(SearchService.navParse(ctrl.searchInput));
     };
 };
 
